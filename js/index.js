2d0ff1e2b4c1d3c7434d9b976d40b91997fb7208
imkrrish <imkrrish227@gmail.com>
2020-12-05 21:05:01 +0530

    fix gallery create and complete about us section

--- js/index.js
+++ js/index.js
@@ -1,28 +1,82 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// Gallery Carousel 
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+const dot_parents = document.querySelector('.controls ul');
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// Buttons 
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+//counter
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-1170 * counter) + 'px)';
+
+// dot active class 
+function dot_next(count) {
+  if (count === 4) {
+    dot_parents.children[2].classList.remove('active');
+    dot_parents.children[0].classList.add('active');
+  }else{
+    dot_parents.children[count-2].classList.remove('active');
+    dot_parents.children[count-1].classList.add('active');
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
+
+function dot_prev(count) {
+  if (count === 0) {
+    dot_parents.children[0].classList.remove('active');
+    dot_parents.children[2].classList.add('active');
+  }else{
+    dot_parents.children[count].classList.remove('active');
+    dot_parents.children[count-1].classList.add('active');
+  }
+}
+
+//Button Listners
+
+nextBtn.addEventListener('click', () => {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = '0.6s ease-in-out';
+  counter++;
+  dot_next(counter);
+  carouselSlide.style.transform = 'translateX(' + (-1170 * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = '0.6s ease-in-out';
+  counter--;
+  dot_prev(counter)
+  carouselSlide.style.transform = 'translateX(' + (-1170 * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+  if (carouselImages[counter].id === 'lastClone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = 'translateX(' + (-1170 * counter) + 'px)';
+  }
+
+  if (carouselImages[counter].id === 'firstClone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-1170 * counter) + 'px)';
+  }
+});
+
+
+// dots listners
+
+document.querySelectorAll('.controls li').forEach(function(indicator , ind){
+  indicator.addEventListener('click', function(){
+    counter = ind+1;
+    document.querySelector('.controls .active').classList.remove('active');
+    indicator.classList.add('active');
+    carouselSlide.style.transition = '0.6s ease-in-out';
+    carouselSlide.style.transform = 'translateX(' + (-1170 * counter) + 'px)';
+  });
+});
