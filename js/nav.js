bfc08ca18807538f97acb9f3c103340379d7e320
imkrrish <imkrrish227@gmail.com>
2021-05-16 23:03:01 +0530

    created responsive navbar and hamburger menu

--- js/nav.js
+++ js/nav.js
@@ -6,19 +6,51 @@ function scrollFunction() {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.getElementById("navbar").classList.add("sticky");
         document.getElementById("hero").classList.add("marginTop");
-        document.getElementById("sticky-logo").style.display = "contents";
 
         var currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
+            document.getElementById("sticky-logo").style.display = "contents";
             document.getElementById("navbar").style.top = "0";
         } else {
             document.getElementById("navbar").style.top = "-100%";
+            document.getElementById("sticky-logo").style.display = "none";
         }
         prevScrollpos = currentScrollPos;
 
     } else {
-        document.getElementById("sticky-logo").style.display = "none";
         document.getElementById("navbar").classList.remove("sticky");
         document.getElementById("hero").classList.remove("marginTop");
+        document.getElementById("sticky-logo").style.display = "none";
+        if (window.innerWidth < 860) {
+            document.getElementById("sticky-logo").style.display = "contents";
+        }
     }
 }
+
+
+window.addEventListener('resize', function () {
+    addRequiredClass();
+})
+
+
+function addRequiredClass() {
+    if (window.innerWidth < 860) {
+        document.getElementById("navbar").classList.add("mobile");
+        document.getElementById("sticky-logo").style.display = "contents";
+    } else {
+        document.getElementById("navbar").classList.remove("mobile");
+        document.getElementById("sticky-logo").style.display = "none";
+    }
+}
+
+window.onload = addRequiredClass
+
+let hamburger = document.querySelector('.hamburger')
+let mobileNav = document.querySelector('.nav-list')
+
+let bars = document.querySelectorAll('.hamburger span')
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('open')
+    mobileNav.classList.toggle('open')
+})
