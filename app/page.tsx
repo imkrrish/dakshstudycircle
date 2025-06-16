b4a3fe255010a664aeff89e744aa1ef2e2e2fe52
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2025-06-16 23:17:08 +0530

    initial next js setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { BentoGridDemo } from "@/components/BentoGridDemo";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <BentoGridDemo />
+      <Button>Hello</Button>
+    </div>
+  );
+}
